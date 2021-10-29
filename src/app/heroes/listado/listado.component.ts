400e641d3ebdd27660f6285800102ced6ce65ee6
angelika <angielikagp@gmail.com>
2021-10-29 15:00:38 -0500

    contador y listado

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spaiderman', 'Goku', 'Hulk', 'Thor'];
+  heroeBorrado : string = '';
+  
+  borrarHeroe() {
+    console.log('Borrando.....');
+    this.heroeBorrado = this.heroes.shift() || ''  ;
+  }
+}
